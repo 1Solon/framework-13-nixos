64fc808866ee70fba82c932e6c5de428ebec875b
1Solon <Solonerus@gmail.com>
2026-06-28 22:49:28 +0200

    Fixes starship home behaviour

--- userspace/starship.nix
+++ userspace/starship.nix
@@ -1,7 +1,17 @@
-{ ... }: {
+{ ... }:
+
+{
   # Starship prompt configuration
   programs.starship = {
     enable = true;
-    settings = { };
+    settings = {
+      command_timeout = 500;
+      scan_timeout = 10;
+      format = "$directory$git_branch$git_status$nix_shell$line_break$character";
+    };
+  };
+
+  home.sessionVariables = {
+    STARSHIP_LOG = "error";
   };
 }
